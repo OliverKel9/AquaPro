d364a73642bd2aed531f0aeb2e0a81e128c82199
Kelton <161046537+OliverKel9@users.noreply.github.com>
2025-11-13 17:36:19 -0300

    Adicionando frontend por partes

---
+++ frontend/projeto/constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#0B5AA2';
+const tintColorDark = '#5DA8FF';
+
+export const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#F4F9FF',
+    tint: tintColorLight,
+    tabIconDefault: '#8A8A8A',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#0B0B0B',
+    tint: tintColorDark,
+    tabIconDefault: '#8A8A8A',
+    tabIconSelected: tintColorDark,
+  },
+};
